5f6af0da7f67ca3a41b9485fba034b1e85470304
mohamedselem <the1ne_manga@yahoo.com>
2023-08-17 16:01:53 -0700

    second task

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers 0 to 14
+*/
+
+void more_numbers(void)
+{
+	int i, k;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (k = 0; k <= 14; k++)
+		{
+			if (k >= 10)
+				_putchar('1');
+			_putchar (k % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
+
