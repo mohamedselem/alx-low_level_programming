02a6338b97483db55fee70ccdcd1f40c9144c1bc
mohamedselem <the1ne_manga@yahoo.com>
2023-08-30 14:17:57 -0700

    6 tasks

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,7 +3,7 @@
 int actual_prime(int n, int i);
 
 /**
- * is_prime_number - FUNC can return the 1 if an integer is a prime number or not
+ * is_prime_number - FUNC can return 1 if an integer is a prime number or not
  * @n: number to evaluate
  *
  * Return: 1 if n is a prime number, 0 if not
